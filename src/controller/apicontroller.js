7632fec4f350dd29a087ff919f1e42129bea03e3
aamoros <aamoros@knowmadmood.com>
2024-11-26 15:17:22 +0100

    add console logs for fly.io

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -1,9 +1,11 @@
 const apiController = {
   verificar: (req, res) => {
     res.send("Verification successful");
+    console.log("Verification Console");
   },
   recibir: (req, res) => {
     res.send("Data received");
+    console.log("Recived Console");
   },
 };
 
